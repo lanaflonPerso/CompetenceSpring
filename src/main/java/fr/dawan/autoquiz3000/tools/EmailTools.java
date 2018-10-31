3c2b061ca929f01b49f83ba0abdfe7f28604a621
vianneyba <vianneyba@free.fr>
2018-10-31 23:19:20 +0100

    Modification de la version apache-mail pour utiliser Gmail changer le mot de passe dans le fichier de config

--- src/main/java/fr/dawan/autoquiz3000/tools/EmailTools.java
+++ src/main/java/fr/dawan/autoquiz3000/tools/EmailTools.java
@@ -1,19 +1,20 @@
 package fr.dawan.autoquiz3000.tools;
 
+import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.SimpleEmail;
 
 public class EmailTools {
-	private static final String SMTP_SERVER = "smtp.dsl.ovh.net";
+//	private static final String SMTP_SERVER = "smtp.dsl.ovh.net";
 	private static final String EMAIL_SENDER = "noreply@gmail.com";
 	
 	public static void sendEmail(String to, String subject, String msg) throws Exception {
 		Email email = new SimpleEmail();
-		email.setHostName(SMTP_SERVER);
-		//email.setHostName("smtp.googlemail.com");
-//		email.setSmtpPort(25);
-//		email.setAuthenticator(new DefaultAuthenticator("username", "password"));
-//		email.setSSLOnConnect(true);
+//		email.setHostName(SMTP_SERVER);
+		email.setHostName("smtp.googlemail.com");
+		email.setSmtpPort(25);
+		email.setAuthenticator(new DefaultAuthenticator("vianneyba", "aChanger"));
+		email.setSSLOnConnect(true);
 		email.setFrom(EMAIL_SENDER);
 		email.setSubject(subject);
 		email.setMsg(msg);
